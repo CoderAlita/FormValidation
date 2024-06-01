6ca9ba5c42ee7ee39233ab4c2f55fd00afc18623
CoderAlita <priyankavisheshwar1018@gmail.com>
2024-06-02 02:09:40 +0530

    add form validations with success page

--- src/main/java/com/example/formvalidation/controller/FormController.java
+++ src/main/java/com/example/formvalidation/controller/FormController.java
@@ -2,9 +2,14 @@ package com.example.formvalidation.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.formvalidation.entity.LoginData;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class FormController {
@@ -15,4 +20,19 @@ public class FormController {
 		model.addAttribute("loginData", new LoginData());
 		return "form";
 	}
+	
+	@PostMapping("/success")
+	public String success(@Valid  @ModelAttribute("loginData") LoginData loginData ,BindingResult result) {
+		
+		if(result.hasErrors()) {
+			
+			System.out.println(result);
+			return "form";
+		}
+		System.out.println(loginData);
+	
+		return "success";
+	}
+	
+
 }
--- src/main/java/com/example/formvalidation/entity/LoginData.java
+++ src/main/java/com/example/formvalidation/entity/LoginData.java
@@ -1,8 +1,21 @@
 package com.example.formvalidation.entity;
 
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class LoginData {
+	
+	@NotBlank(message="UserName cannot be empty!!")
+	@Size(min=3, max=10,message="Username must be 3-10 cahracters !!!")
 	private String userName;
+	
+	@NotBlank(message="Email cannot be empty!!")
+	@Pattern(regexp="^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@gmail.com+$", message="please enter a valid email id !!!")
 	private String email;
+	@AssertTrue(message="Please agree the terms & conditions!!")
+	private boolean agreed;
 	public String getUserName() {
 		return userName;
 	}
@@ -14,6 +27,12 @@ public class LoginData {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	public boolean isAgreed() {
+		return agreed;
+	}
+	public void setAgreed(boolean agreed) {
+		this.agreed = agreed;
 	}
 	@Override
 	public String toString() {
